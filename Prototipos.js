c1d00342184bf07c0fac758458174c100f463459
VictorHugoRoman <VictorHugoRoman@users.noreply.github.com>
2018-04-13 14:21:14 -0600

    Update Prototipos.js

--- Prototipos.js
+++ Prototipos.js
@@ -1,12 +1,21 @@
+//para crear prototipos en javascript
+//primero creamos una funcion que reciba los atributos
 function Punto(x, y) {
   this.x = x
   this.y = y
 }
+//keyword "new" seguida de una funcion nos crea un objeto, le asigna el prototipo que
+//le definamos ej: const p2 = new Punto(3, 0), ejecuta el cuerpo del metodo, retorna el objeto
 
 Punto.prototype.moverEnX = function moverEnX(x) {
-  this.x += x
+  this.x += x; //this hace referencia al objeto que javascript esta creando
 }
+//para retornar el objeto no es necesario usar el return
+//ya que javascript lo retorna implicitamente con el this
 
+
+//con Punto.prototype le agregamos los metodos al prototipo, le podemos poner o no nombre a las funcions
+//es recomendable para que javascript nos el nombre de la funcion en caso que nos de errror
 Punto.prototype.moverEnY = function moverEnY(y) {
   this.y += y
 }
